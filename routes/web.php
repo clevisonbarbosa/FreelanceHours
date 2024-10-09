4a1fc9c19515db63f425499263bd8658bbaa7609
Clévison barbosa <50533700+clevisonbarbosa@users.noreply.github.com>
2024-10-09 20:17:47 -0300

    criacao dos primeiros components livewire

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
+Route::view('/', 'projects.index')->name('projects.index');
+Route::view('/projects/{project}', 'projects.show')->name('projects.show');
